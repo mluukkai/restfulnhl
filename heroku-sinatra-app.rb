bc42228c6448747e6405530371ad2ab5ea35dc4e
Matti Luukkainen <mluukkai@iki.fi>
2011-03-05 13:00:42 +0200

    aa

--- heroku-sinatra-app.rb
+++ heroku-sinatra-app.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'statistics'
+require 'formatter'
 
 require 'dm-core'
 require 'dm-migrations'
@@ -102,46 +103,3 @@ class SWSApp < Sinatra::Base
 	end
 end
 
-class Formatter
-  def self.goal
-    GoalFormatter.new
-  end
-
-  def self.assist
-    AssistFormatter.new
-  end
-
-  def self.point
-    PointFormatter.new
-  end
-
-  def points point
-    point
-  end
-
-  def assists assist
-    assist
-  end
-
-  def goals goal
-    goal
-  end
-end
-
-class GoalFormatter < Formatter
-  def goals goal
-    "<b>#{goal}</b>"
-  end
-end
-
-class AssistFormatter < Formatter
-  def assists assist
-    "<b>#{assist}</b>"
-  end
-end
-
-class PointFormatter < Formatter
-  def points point
-    "<b>#{point}</b>"
-  end
-end
