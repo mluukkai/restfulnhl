ad11ebe563f37969af0d8accd201f8978dfce168
Matti Luukkainen <mluukkai@iki.fi>
2011-03-05 13:14:03 +0200

    aa

--- heroku-sinatra-app.rb
+++ heroku-sinatra-app.rb
@@ -24,29 +24,8 @@ class SWSApp < Sinatra::Base
 	  DataMapper.auto_upgrade!
 	end
 
-	get '/drop' do
-	  DataMapper.auto_migrate!
-	  "all dropped <a href=\"/\">back</a>"
-	end
-
 	get '/' do
     redirect '/points'
-  end
-
-  get '/old_index' do
-    redirect '/points'
-
-	  @nro = Numero.first(:id => 1)
-
-	  @nro = Numero.new unless @nro  # jos ei löytynyt id:llä "1", niin luo uusi
-
-	  @nro.amount = @nro.amount + 1   # kasvata normaalisti kenttää
-	  @nro.save                       # persistoi
-
-    stats = Statistics.new( NHLParser.new )
-    @point_order = stats.point_order
-    #"#{stats.print_in_goals_order}"
-    erb :index
   end
 
   def get_points_order
@@ -91,6 +70,29 @@ class SWSApp < Sinatra::Base
 
 	get '/env' do
 	  ENV.inspect
+  end
+
+  # legacy starts, forget these:
+
+  get '/drop' do
+	  DataMapper.auto_migrate!
+	  "all dropped <a href=\"/\">back</a>"
 	end
+
+  get '/old_index' do
+    redirect '/points'
+
+	  @nro = Numero.first(:id => 1)
+
+	  @nro = Numero.new unless @nro  # jos ei löytynyt id:llä "1", niin luo uusi
+
+	  @nro.amount = @nro.amount + 1   # kasvata normaalisti kenttää
+	  @nro.save                       # persistoi
+
+    stats = Statistics.new( NHLParser.new )
+    @point_order = stats.point_order
+    #"#{stats.print_in_goals_order}"
+    erb :index
+  end
 end
 
